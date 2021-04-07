7e7ab62ce1104823b4e99443887170c5364bd731
Ratchadaaa <ratchaporn_pat@vu.ac.th>
2021-04-07 11:58:10 +0700

    Delete unused files by Ada

--- src/newweb/admin/modules/user/function/delete.php
+++
@@ -1,9 +0,0 @@
-<?
-		session_start();
-		include "../../../libraries/config/config.php";
-		$view1=$_REQUEST['delID'];
-		$check=mysql_query("delete from tb_administrator where user_id='$view1'");
-		if($check){echo "<script language='JavaScript' type='text/javascript'> window.parent.refreshPage();</script>"; }
-		
-		
-?>
