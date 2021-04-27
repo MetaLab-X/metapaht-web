7ee470d9bc8d589bd9c0d77ac6b6982ca69d70ba
Ratchadaaa <ratchaporn_pat@vu.ac.th>
2021-04-27 19:17:42 +0700

    Add activities on timeline May-July

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -8,14 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CalenderComponent implements OnInit {
 
   constructor() { }
-  events: any[] = [];
-  ngOnInit() {
-    this.events = [
-      { content: 'Ordered', date: '15/02/2021 10:30', status: 'R' },
-      { content: 'Processing', date: '15/02/2021 14:00', status: 'R' },
-      { content: 'Shipped', status: 'R', date: '15/02/2021 17:00' },
-      { content: 'Delivered' },
-    ]
+  
+  ngOnInit(): void {
   }
 
   }
